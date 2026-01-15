b18219c3c361e988e5a57162bfc38591fd1556b8
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2026-01-15 22:45:17 +0530

    nitpick: api change

--- src/routes/fetch_by_id.rs
+++ src/routes/fetch_by_id.rs
@@ -1,7 +1,7 @@
 use axum::{routing::get, Router};
 use sea_orm::DatabaseConnection;
 
-use crate::handlers::{
+use crate::{handlers::{
     fetch_by_id::{
         get_farmer_commission_history_by_id_handler, get_stock_return_unit_cost,
         get_stock_returns_by_batch_id_handler,
@@ -10,7 +10,7 @@ use crate::handlers::{
         get_supplier_ledger_handler, get_supplier_payables, get_supplier_payments_byid_handler,
     },
     traders::{get_trader_ledger_handler, get_trader_payments, get_trader_receivables},
-};
+}, pdf::draw_header::generate_pdf_handler};
 
 pub fn fetch_by_id() -> Router<DatabaseConnection> {
     Router::new()
@@ -35,4 +35,5 @@ pub fn fetch_by_id() -> Router<DatabaseConnection> {
         .route("/trader_receivables/{id}", get(get_trader_receivables))
         .route("/trader_payments/{id}", get(get_trader_payments))
         .route("/trader_ledger/{id}", get(get_trader_ledger_handler))
+        .route("/growing_charges/{id}", get(generate_pdf_handler))
 }
